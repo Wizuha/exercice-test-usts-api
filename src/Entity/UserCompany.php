e0b53881d55ac16f2067f5fa2dd30d6f14b5e4ef
Wizuha <williams.wandji@gmail.com>
2024-10-16 12:05:58 +0200

    correction assert importation in usercompany entity

--- src/Entity/UserCompany.php
+++ src/Entity/UserCompany.php
@@ -6,6 +6,7 @@ use ApiPlatform\Metadata\ApiResource;
 use ApiPlatform\Metadata\Post;
 use Symfony\Component\Serializer\Annotation\Groups;
 use App\Repository\UserCompanyRepository;
+use Symfony\Component\Validator\Constraints as Assert;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: UserCompanyRepository::class)]
@@ -68,13 +69,13 @@ class UserCompany
 
         return $this;
     }
-
+    
     #[Assert\Choice(choices: ['admin', 'manager', 'consultant'], message: 'Invalid role')]
     public function getRole(): ?string
     {
         return $this->role;
     }
-
+    
     public function setRole(string $role): self
     {
         $this->role = $role;
